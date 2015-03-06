19982184e1fed70d737d4a1f454fcbfeafa6fab1
Ian Rose <ian.r.rose@gmail.com>
2015-03-05 18:59:52 -0800

    Continue setting up source branch

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ian Rose'
+SITENAME = u'Ian Rose'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+
+DEFAULT_LANG = u'en'
+STATIC_PATHS=['interactive_earth']
+READERS = {'html':None }
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
