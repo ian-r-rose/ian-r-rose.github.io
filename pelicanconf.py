5bd11a5c7dc6df43bbdd81a828812ea594208473
ian-r-rose <ian.r.rose@gmail.com>
2015-08-21 18:36:54 -0700

    Add siteurl

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Ian Rose'
 SITENAME = u'Geomusings'
 SITESUBTITLE = u'Thoughts on planetary science, fluid dynamics, and scientific computing'
-SITEURL=''
+SITEURL='ian-r-rose.github.io'
 PROFILE_IMAGE_URL='/images/ian_rose.jpg'
 
 PATH = 'content'
