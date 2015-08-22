3f48ce88e84ebe871122db47b6f4d48f64af1b30
ian-r-rose <ian.r.rose@gmail.com>
2015-08-21 17:44:31 -0700

    Update configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,10 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Ian Rose'
-SITENAME = u'Ian Rose'
-SITEURL = ''
+SITENAME = u'Geomusings'
+SITESUBTITLE = u'Thoughts on planetary science, fluid dynamics, and scientific computing'
+SITEURL=''
+PROFILE_IMAGE_URL='/images/ian_rose.jpg'
 
 PATH = 'content'
 
@@ -12,7 +14,7 @@ TIMEZONE = 'America/Los_Angeles'
 
 
 DEFAULT_LANG = u'en'
-STATIC_PATHS=['interactive_earth']
+STATIC_PATHS=['interactive_earth', 'images']
 READERS = {'html':None }
 
 # Feed generation is usually not desired when developing
@@ -25,5 +27,19 @@ AUTHOR_FEED_RSS = None
 
 DEFAULT_PAGINATION = 10
 
+MENUITEMS=[('blog', '/'), ('interactive earth', '/interactive_earth/index.html')]
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME='pelican-crowsfoot'
+#THEME='pelican-octopress-theme'
+PLUGIN_PATHS=['pelican-plugins',]
+PLUGINS = ['summary', \
+           'liquid_tags.img',\
+           'liquid_tags.include_code', \
+           'liquid_tags.notebook',\
+           'liquid_tags.literal']
+
+GITHUB_ADDRESS = 'http://github.com/ian-r-rose'
+TWITTER_ADDRESS = 'http://twitter.com/IanRRose'
